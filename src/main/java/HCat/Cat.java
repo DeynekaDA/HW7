5120d074a081086485f91e1676617e41b960eb0c
Denis <den9695@list.ru>
2021-04-08 15:23:16 +0300

    My HW

--- src/main/java/HCat/Cat.java
+++ src/main/java/HCat/Cat.java
@@ -2,4 +2,43 @@ package HCat;
 
 public class Cat {
 
+    private final String name;
+    private boolean hungry;
+    private final int appetite;
+
+    public Cat(String name, int appetite) {
+        this.name = name;
+        this.appetite = appetite;
+        hungry = true;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAppetite() {
+        return appetite;
+    }
+
+    public boolean isHungry() {
+        return hungry;
+    }
+
+    public void eat(Plate plate){
+        if (!hungry) {
+            System.out.println(this + " не голоден");
+            return;
+        }
+        hungry = !plate.eat(appetite);
+        if (hungry) {
+            System.out.println("Нет еды");
+        } else {
+            System.out.println(this + " поел " + appetite + " ед. еды");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return name ;
+    }
 }
